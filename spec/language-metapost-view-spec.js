055fe95ecf204f712011530551977f17886ee057
khiri-co <khiri.co@gmail.com>
2016-08-14 14:01:10 +0900

    first commit.

---
+++ spec/language-metapost-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageMetapostView from '../lib/language-metapost-view';
+
+describe('LanguageMetapostView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
